c7a3be069e456e12fbbd6224248a7c26e62401ed
Anika <anikafarzana@iut-dhaka.edu>
2023-10-14 03:50:13 +0600

    sidebar hamburger click open close

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -11,4 +11,30 @@ $(document).ready(() => {
         Events = document.querySelector('.Events');
         Events.classList.add('active');
     }
+    // $.ajax({
+    //     type: 'get',
+    //     url: $(form).attr('action'),
+    //     data: $(form).serialize(),
+    //     dataType: 'json'
+    // })
+    // .done(function (response) {
+        
+    // });
+    open =0;
+});
+
+hamburger = document.querySelector('.hamburger');
+sidebar = document.querySelector('.sidebar');
+hamburger.addEventListener('click', () => {
+    // sidebar.classList.add('open');
+    if(open==0){
+        sidebar.style.width= "240px";
+        sidebar.style.transition = "0.5s";
+        open=1;
+    }
+    else if(open==1){
+        sidebar.style.width= "108px";
+        sidebar.style.transition = "0.5s";
+        open=0;
+    }
 });
